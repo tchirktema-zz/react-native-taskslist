bee602a96bec08238beb1bad243a19adac6e5012
Josue Tchirktema <tchirktemajosue@gmail.com>
2021-12-14 14:51:20 +0000

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import TasksScreen from './src/screens/Tasks';
+import {Provider} from 'react-redux'
+import {store} from './src/redux/store'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+       <SafeAreaView style={{flex:1}}>
+        <TasksScreen/>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
